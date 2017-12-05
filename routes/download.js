1f1fe8dd6f0df8f05c79bee955c3d3c7d49d42f8
krishnasameeth <krishna.sameeth@gmail.com>
2017-12-05 17:03:44 +0530

    "made changes to download.js"

--- routes/download.js
+++ routes/download.js
@@ -6,138 +6,170 @@ router.post('/zip', function(req, res){
     console.log('express request');
     // console.log(req.body.authentication);
 
-    const authentication =req.body.authentication
-    switch(authentication) {
-        case 'facebook':
-            shell.exec(
-                // 'rm -rf duplicate/ && rsync -arv --exclude={ReactExpress/node_modules,ReactExpress/reactApp/node_modules} ReactExpress duplicate && zip -r ReactExpress.zip duplicate'
-                'rm -rf duplicate/ && rsync -arv --exclude=ReactExpress/node_modules --exclude=ReactExpress/reactApp/node_modules ReactExpress duplicate/ '
-                , function(code, stdout, stderr) {
-                    if (stderr) {
-                        // shell.echo('Error: Git commit failed');
-                        // shell.exit(1);
-                    }
-                    console.log(arguments)
-                    shell.exec(
-                        // 'rm -rf duplicate/ && rsync -arv --exclude={ReactExpress/node_modules,ReactExpress/reactApp/node_modules} ReactExpress duplicate && zip -r ReactExpress.zip duplicate'
-                        "sed -i '/facebook start/,/facebook end/d' duplicate/ReactExpress/routes/auth.js"
 
-                        , function(code, stdout, stderr) {
-                            if (stderr) {
-                                console.log(stderr)
-                                exit
-                            }
-                            console.log(arguments)
+  var commands =[];
+  commands[0]= 'rm -rf duplicate/ && rsync -arv --exclude=ReactExpress/node_modules --exclude=ReactExpress/reactApp/node_modules ReactExpress duplicate/ '
 
+  if(!req.body.facebook){
+    commands.push("sed -i '/facebook start/,/facebook end/d' duplicate/ReactExpress/routes/auth.js");
+  }
+	if(!req.body.google){
+		commands.push("sed -i '/google start/,/google end/d' duplicate/ReactExpress/routes/auth.js");
+	}
+	if(!req.body.github){
+		commands.push("sed -i '/github start/,/github end/d' duplicate/ReactExpress/routes/auth.js");
+	}
 
-                            shell.exec(
-                                // 'rm -rf duplicate/ && rsync -arv --exclude={ReactExpress/node_modules,ReactExpress/reactApp/node_modules} ReactExpress duplicate && zip -r ReactExpress.zip duplicate'
-                                'zip -r ReactExpress.zip duplicate  -x "ReactExpress/node_modules/*" "ReactExpress/reactApp/node_modules/*"'
-                                , function(code, stdout, stderr) {
-                                    if (stderr) {
-                                        // shell.echo('Error: Git commit failed');
-                                        // shell.exit(1);
-                                    }
-                                    console.log(arguments)
+	commands.push('zip -r ReactExpress.zip duplicate  -x "ReactExpress/node_modules/*" "ReactExpress/reactApp/node_modules/*"');
 
-                                    var file = __dirname + '/../ReactExpress.zip';
-                                    res.download(file)
-                                });
-
-                        });
-
-                });
-            console.log('facebook');
-            break;
-        case 'google':
-            shell.exec(
-                // 'rm -rf duplicate/ && rsync -arv --exclude={ReactExpress/node_modules,ReactExpress/reactApp/node_modules} ReactExpress duplicate && zip -r ReactExpress.zip duplicate'
-                'rm -rf duplicate/ && rsync -arv --exclude=ReactExpress/node_modules --exclude=ReactExpress/reactApp/node_modules ReactExpress duplicate/ '
-                , function(code, stdout, stderr) {
-                    if (stderr) {
-                        // shell.echo('Error: Git commit failed');
-                        // shell.exit(1);
-                    }
-                    console.log(arguments)
-                    shell.exec(
-                        // 'rm -rf duplicate/ && rsync -arv --exclude={ReactExpress/node_modules,ReactExpress/reactApp/node_modules} ReactExpress duplicate && zip -r ReactExpress.zip duplicate'
-                        "sed -i '/google start/,/google end/d' duplicate/ReactExpress/routes/auth.js"
-
-                        , function(code, stdout, stderr) {
-                            if (stderr) {
-                                console.log(stderr)
-                                exit
-                            }
-                            console.log(arguments)
-
-
-                            shell.exec(
-                                // 'rm -rf duplicate/ && rsync -arv --exclude={ReactExpress/node_modules,ReactExpress/reactApp/node_modules} ReactExpress duplicate && zip -r ReactExpress.zip duplicate'
-                                'zip -r ReactExpress.zip duplicate  -x "ReactExpress/node_modules/*" "ReactExpress/reactApp/node_modules/*"'
-                                , function(code, stdout, stderr) {
-                                    if (stderr) {
-                                        // shell.echo('Error: Git commit failed');
-                                        // shell.exit(1);
-                                    }
-                                    console.log(arguments)
-
-                                    var file = __dirname + '/../ReactExpress.zip';
-                                    res.download(file)
-                                });
-
-                        });
-
-                });
-            console.log('google');
-            break;
-        case 'github':
-            shell.exec(
-                // 'rm -rf duplicate/ && rsync -arv --exclude={ReactExpress/node_modules,ReactExpress/reactApp/node_modules} ReactExpress duplicate && zip -r ReactExpress.zip duplicate'
-                'rm -rf duplicate/ && rsync -arv --exclude=ReactExpress/node_modules --exclude=ReactExpress/reactApp/node_modules ReactExpress duplicate/ '
-                , function(code, stdout, stderr) {
-                if (stderr) {
-                    // shell.echo('Error: Git commit failed');
-                    // shell.exit(1);
-                }
-                console.log(arguments)
-                    shell.exec(
-                        // 'rm -rf duplicate/ && rsync -arv --exclude={ReactExpress/node_modules,ReactExpress/reactApp/node_modules} ReactExpress duplicate && zip -r ReactExpress.zip duplicate'
-                        "sed -i '/github start/,/github end/d' duplicate/ReactExpress/routes/auth.js"
-
-                        , function(code, stdout, stderr) {
-                            if (stderr) {
-                                console.log(stderr)
-                                exit
-                            }
-                            console.log(arguments)
-
-
-                            shell.exec(
-                                // 'rm -rf duplicate/ && rsync -arv --exclude={ReactExpress/node_modules,ReactExpress/reactApp/node_modules} ReactExpress duplicate && zip -r ReactExpress.zip duplicate'
-                                'zip -r ReactExpress.zip duplicate  -x "ReactExpress/node_modules/*" "ReactExpress/reactApp/node_modules/*"'
-                                , function(code, stdout, stderr) {
-                                    if (stderr) {
-                                        // shell.echo('Error: Git commit failed');
-                                        // shell.exit(1);
-                                    }
-                                    console.log(arguments)
-
-                                    var file = __dirname + '/../ReactExpress.zip';
-                                    res.download(file)
-                                });
-
-                        });
-
-            });
-            console.log('github');
-            break;
-        default:
-            console.log('all');
-    }
-
-    res.json('downaloding..')
-
+	for(var i=0;i<commands.length;i++){
+	  shell.exec(commands[i],function(code,stdout,stderr){
+		  if(stderr) {
+			  // shell.echo('Error: Git commit failed');
+			  // shell.exit(1);
+		  }
+		  console.log(i);
+		  if(i === 2){
+			  var file = __dirname + '/../ReactExpress.zip';
+			  res.download(file);
+		  }
+    });
+	}
 });
+
+//
+// 	const authentication =req.body.authentication
+//     switch(authentication) {
+//         case 'facebook':
+//             shell.exec(
+//                 // 'rm -rf duplicate/ && rsync -arv --exclude={ReactExpress/node_modules,ReactExpress/reactApp/node_modules} ReactExpress duplicate && zip -r ReactExpress.zip duplicate'
+//                 'rm -rf duplicate/ && rsync -arv --exclude=ReactExpress/node_modules --exclude=ReactExpress/reactApp/node_modules ReactExpress duplicate/ '
+//                 , function(code, stdout, stderr) {
+//                     if (stderr) {
+//                         // shell.echo('Error: Git commit failed');
+//                         // shell.exit(1);
+//                     }
+//                     console.log(arguments)
+//                     shell.exec(
+//                         // 'rm -rf duplicate/ && rsync -arv --exclude={ReactExpress/node_modules,ReactExpress/reactApp/node_modules} ReactExpress duplicate && zip -r ReactExpress.zip duplicate'
+//                         "sed -i '/facebook start/,/facebook end/d' duplicate/ReactExpress/routes/auth.js"
+//
+//                         , function(code, stdout, stderr) {
+//                             if (stderr) {
+//                                 console.log(stderr)
+//                                 exit
+//                             }
+//                             console.log(arguments)
+//
+//
+//                             shell.exec(
+//                                 // 'rm -rf duplicate/ && rsync -arv --exclude={ReactExpress/node_modules,ReactExpress/reactApp/node_modules} ReactExpress duplicate && zip -r ReactExpress.zip duplicate'
+//                                 'zip -r ReactExpress.zip duplicate  -x "ReactExpress/node_modules/*" "ReactExpress/reactApp/node_modules/*"'
+//                                 , function(code, stdout, stderr) {
+//                                     if (stderr) {
+//                                         // shell.echo('Error: Git commit failed');
+//                                         // shell.exit(1);
+//                                     }
+//                                     console.log(arguments)
+//
+//                                     var file = __dirname + '/../ReactExpress.zip';
+//                                     res.download(file)
+//                                 });
+//
+//                         });
+//
+//                 });
+//             console.log('facebook');
+//             break;
+//         case 'google':
+//             shell.exec(
+//                 // 'rm -rf duplicate/ && rsync -arv --exclude={ReactExpress/node_modules,ReactExpress/reactApp/node_modules} ReactExpress duplicate && zip -r ReactExpress.zip duplicate'
+//                 'rm -rf duplicate/ && rsync -arv --exclude=ReactExpress/node_modules --exclude=ReactExpress/reactApp/node_modules ReactExpress duplicate/ '
+//                 , function(code, stdout, stderr) {
+//                     if (stderr) {
+//                         // shell.echo('Error: Git commit failed');
+//                         // shell.exit(1);
+//                     }
+//                     console.log(arguments)
+//                     shell.exec(
+//                         // 'rm -rf duplicate/ && rsync -arv --exclude={ReactExpress/node_modules,ReactExpress/reactApp/node_modules} ReactExpress duplicate && zip -r ReactExpress.zip duplicate'
+//                         "sed -i '/google start/,/google end/d' duplicate/ReactExpress/routes/auth.js"
+//
+//                         , function(code, stdout, stderr) {
+//                             if (stderr) {
+//                                 console.log(stderr)
+//                                 exit
+//                             }
+//                             console.log(arguments)
+//
+//
+//                             shell.exec(
+//                                 // 'rm -rf duplicate/ && rsync -arv --exclude={ReactExpress/node_modules,ReactExpress/reactApp/node_modules} ReactExpress duplicate && zip -r ReactExpress.zip duplicate'
+//                                 'zip -r ReactExpress.zip duplicate  -x "ReactExpress/node_modules/*" "ReactExpress/reactApp/node_modules/*"'
+//                                 , function(code, stdout, stderr) {
+//                                     if (stderr) {
+//                                         // shell.echo('Error: Git commit failed');
+//                                         // shell.exit(1);
+//                                     }
+//                                     console.log(arguments)
+//
+//                                     var file = __dirname + '/../ReactExpress.zip';
+//                                     res.download(file)
+//                                 });
+//
+//                         });
+//
+//                 });
+//             console.log('google');
+//             break;
+//         case 'github':
+//             shell.exec(
+//                 // 'rm -rf duplicate/ && rsync -arv --exclude={ReactExpress/node_modules,ReactExpress/reactApp/node_modules} ReactExpress duplicate && zip -r ReactExpress.zip duplicate'
+//                 'rm -rf duplicate/ && rsync -arv --exclude=ReactExpress/node_modules --exclude=ReactExpress/reactApp/node_modules ReactExpress duplicate/ '
+//                 , function(code, stdout, stderr) {
+//                 if (stderr) {
+//                     // shell.echo('Error: Git commit failed');
+//                     // shell.exit(1);
+//                 }
+//                 console.log(arguments)
+//                     shell.exec(
+//                         // 'rm -rf duplicate/ && rsync -arv --exclude={ReactExpress/node_modules,ReactExpress/reactApp/node_modules} ReactExpress duplicate && zip -r ReactExpress.zip duplicate'
+//                         "sed -i '/github start/,/github end/d' duplicate/ReactExpress/routes/auth.js"
+//
+//                         , function(code, stdout, stderr) {
+//                             if (stderr) {
+//                                 console.log(stderr)
+//                                 exit
+//                             }
+//                             console.log(arguments)
+//
+//
+//                             shell.exec(
+//                                 // 'rm -rf duplicate/ && rsync -arv --exclude={ReactExpress/node_modules,ReactExpress/reactApp/node_modules} ReactExpress duplicate && zip -r ReactExpress.zip duplicate'
+//                                 'zip -r ReactExpress.zip duplicate  -x "ReactExpress/node_modules/*" "ReactExpress/reactApp/node_modules/*"'
+//                                 , function(code, stdout, stderr) {
+//                                     if (stderr) {
+//                                         // shell.echo('Error: Git commit failed');
+//                                         // shell.exit(1);
+//                                     }
+//                                     console.log(arguments)
+//
+//                                     var file = __dirname + '/../ReactExpress.zip';
+//                                     res.download(file)
+//                                 });
+//
+//                         });
+//
+//             });
+//             console.log('github');
+//             break;
+//         default:
+//             console.log('all');
+//     }
+//
+//     res.json('downaloding..')
+//
+// });
 
 router.get('/expressHtml', function(req, res){
     var file = __dirname + '/../expressHtml.zip';
