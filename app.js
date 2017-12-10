369bded445bfa6991b1fdd832c293a81fe31d552
ak-zul65 <ak.zul65@gmaill.com>
2017-12-11 01:31:15 +0530

    again change port as 10001

--- app.js
+++ app.js
@@ -26,7 +26,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/download',download);
-
+app.use('/api/*',function(req,res){
+  console.log( req.originalUrl)
+  res.redirect(req.originalUrl.split("api"))
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
