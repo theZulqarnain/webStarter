1df99bedf766507da558c52909ed02aad3c5dd05
ak-zul65 <ak.zul65@gmaill.com>
2017-12-11 15:14:09 +0530

    again change port as 10001

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use('/', index);
 app.use('/users', users);
 app.use('/download',download);
 app.use('/api/*',function(req,res){
-  console.log( req.originalUrl.split("/api")[1])
+  console.log("*", req.originalUrl.split("/api")[1])
   res.redirect(req.originalUrl.split("/api")[1])
 });
 
